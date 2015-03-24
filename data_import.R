8e5925c732f88e28660bdc8279ea400c33e2e37f
Francois Michonneau <francois.michonneau@gmail.com>
2015-03-24 17:25:06 -0400

    code for latest collections import

--- data_import.R
+++ data_import.R
@@ -2,15 +2,28 @@ setwd("~/Documents/iDigBio/PhotoImport")
 source("~/Documents/iDigBio/idigbio-ingestion-data-preparation/data_functions.R")
 ufdb <- read.csv(file="allUF.csv", stringsAsFactors=FALSE)
 load("guidm.RData")
+
+nlines <- unlist(strsplit(system("wc -l occurrence.txt", intern=TRUE), "\\s"))[1]
 guidFull <- read.csv(file="occurrence.txt", row.names=NULL,
-                     header=FALSE, nrows=535366, stringsAsFactors=FALSE)
-guid <- guidFull[, c(1, 17)]
+                     header=TRUE, nrows=as.numeric(nlines), stringsAsFactors=FALSE)
+guid <- guidFull[, c("id", "dwc.catalogNumber")]
 names(guid) <- c("idigbio-guid", "ufid")
+
+## library(RMySQL)
+## con <- dbConnect(MySQL(), user="root", password="root",
+##                  host="localhost", client.flag=CLIENT_MULTI_RESULTS,
+##                  dbname="UFDB_iDigBio")
+## guidQuery<- paste("SELECT `idigbio:uuid`,`dwc:catalogNumber`",
+##                   "FROM occurrence",
+##                   "WHERE `dwc:catalogNumber` REGEXP '.{1,}';")
+## guid <- dbGetQuery(con, statement=guidQuery)
+
+
+
+
 
 ### ------ for dNORS (Northern Red Sea)
 
-###    *********  Not yet finished *********
-###    missing iDigBio GUID for these specimens
 checkPhotoFolder(path="~/Photos/tmp/dNORS_2013_1s")
 checkPhotoInfoFile(file="dNORS/dNORS-photoInfo.csv")
 
@@ -22,6 +35,8 @@ photoDB <- read.csv(file="dNORS/dNORS-photoInfo.csv",
 matchGUID(toMatch, photoDB, ufDB, guid, file="dNORS-matched.csv")
 
 ### ------ for dSTM (St Martin)
+
+### Uploaded successfully on 2014-05-27
 
 extensionToUpper("~/Photos/tmp/dSTM")
 changeSeparator("~/Photos/tmp/dSTM")
@@ -66,3 +81,49 @@ photoDB <- read.csv(file="dMOO10/dMOO10-photoInfo.csv",
 photoDB <- subset(photoDB, subset= photo_quality == "1")
 
 matchGUID(toMatch, photoDB, ufDB, guid, file="dMOO10-matched.csv")
+
+### ------ for dMOO08 (Moorea 2009)
+
+extensionToUpper("~/Photos/tmp/dMOO08")
+changeSeparator("~/Photos/tmp/dMOO08")
+checkPhotoFolder("~/Photos/tmp/dMOO08")
+
+### ------ for NIN09 (redo)
+
+checkPhotoFolder(path="~/Photos/1")
+
+checkPhotoInfoFile("Ningaloo/dNIN09-photoInfo.csv")
+
+toMatch <- read.csv(file="Ningaloo/dNIN09-toBeMatched.csv",
+                    stringsAsFactors=FALSE, check.names=FALSE)
+photoDB <- read.csv(file="Ningaloo/dNIN09-photoInfo.csv",
+                    stringsAsFactors=FALSE, check.names=FALSE)
+matchGUID(toMatch, photoDB, ufdb, guid, file="dNIN09-matched.csv")
+
+
+### ------ for dMOO06 (Moorea 2006)
+### ------ done on 2015-03-24
+
+extensionToUpper("~/hdd/tmp-images-for-iDigBio/Moorea_2006_select/JPGS/")
+changeSeparator("~/hdd/tmp-images-for-iDigBio/Moorea_2006_select/JPGS/")
+checkPhotoFolder("~/hdd/tmp-images-for-iDigBio/Moorea_2006_select/JPGS/")
+
+toMatch <- read.csv(file = "dMOO06/dMOO06-toBeMatched.csv",
+                    stringsAsFactors =  FALSE, check.names = FALSE)
+photoDB <- read.csv(file = "dMOO06/dMOO06-photoInfo.csv",
+                    stringsAsFactors = FALSE, check.names = FALSE)
+matchGUID(toMatch, photoDB, ufdb, guid, file = "dMOO06/dMOO06-matched.csv")
+
+### ------ for FHL
+### ------ done on 2015-03-24
+### note this combines FHL2007 2009 and 2011
+
+extensionToUpper("~/hdd/tmp-images-for-iDigBio/FHL")
+changeSeparator("~/hdd/tmp-images-for-iDigBio/FHL")
+checkPhotoFolder("~/hdd/tmp-images-for-iDigBio/FHL")
+
+toMatch <- read.csv(file = "FHL/dFHL-toBeMatched.csv", stringsAsFactors = FALSE,
+                    check.names = FALSE)
+photoDB <- read.csv(file = "FHL/dFHL-photoInfo.csv",  stringsAsFactors = FALSE,
+                    check.names = FALSE)
+matchGUID(toMatch,  photoDB,  ufdb,  guid, file = "FHL/dFHL-matched.csv")
